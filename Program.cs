d5f9c466f1205d2f49cbe5678a23aa98d5176201
Yuchong Pan <panyuchong@gmail.com>
2019-07-25 19:15:23 -0700

    Added output of first 10 <Message>s

--- Program.cs
+++ Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging;
+using Microsoft.Build.Logging.StructuredLogger;
 
 namespace MSBuildBinLogPlayground
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                PrintErrorMessage("Argument missing");
+            }
+
+            foreach (var binLogFilePath in args)
+            {
+                var binLogReader = new BinLogReader();
+
+                IEnumerable<Record> records;
+                try
+                {
+                    records = binLogReader.ReadRecords(binLogFilePath);
+                }
+                catch (Exception e)
+                {
+                    PrintErrorMessage($"Exception while reading binlog: {e.Message}");
+                    return;
+                }
+
+                // print the first 10 <Message> events
+
+                var count = 0;
+
+                foreach (var record in records)
+                {
+                    if (record.Args is BuildMessageEventArgs buildMessageEventArgs)
+                    {
+                        Console.WriteLine(buildMessageEventArgs.Message);
+
+                        if (++count == 10) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void PrintErrorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
